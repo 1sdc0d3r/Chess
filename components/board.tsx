966d088b68ae482bff06949b054bd2a6e67cfb28
Braden Bell <fish0859@gmail.com>
2021-10-10 14:08:17 -0600

    copied board.tsx from tic-tac-toe game for base board

---
+++ components/board.tsx
@@ -0,0 +1,40 @@
+//! BOARD COPIED FROM TIC-TAC-TOE NO OTHER CHANGES
+export default function Board(props: any) {
+  const tileCount: number = props.tileCount;
+  const tileState: any = props.tileState; //? typescript any->object
+  const player: number = props.player;
+  const toggleTile: (player: number, tileNumber: number) => void =
+    props.toggleTile;
+
+  const tiles = Array.from(Array(tileCount).keys(), (i) => i + 1); //creates array starting with 1
+  return (
+    <div className="board-wrapper w-3/4 h-3/4 border-2 border-solid border-black flex flex-wrap m-auto ">
+      {tiles.map((e, i) => tile(e, i, tileState[e], player))}
+    </div>
+  );
+
+  function tile(
+    tileNumber: number,
+    iterator: number,
+    state: string,
+    player: number
+  ) {
+    //   const color = iterator % 2 ? 'black' : 'white';
+    // console.log({ tileNumber, state });
+    return (
+      <div
+        className={`tile test-border w-1/3 h-1/3 flex overflow-hidden`}
+        key={iterator}
+        onClick={() => toggleTile(player, tileNumber)}
+      >
+        <p
+          className={`${
+            state === 'O' ? 'text-red-600' : 'text-blue-600'
+          } text-9xl w-win h-min m-auto text-center select-none`}
+        >
+          {state}
+        </p>
+      </div>
+    );
+  }
+}
